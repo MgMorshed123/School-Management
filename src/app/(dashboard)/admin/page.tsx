7b6bd49b78a4de54c6195629f7bbabf2796617c8
mdarf <mdarfinji45@gmail.com>
2024-11-04 02:39:16 +0800

    admin page layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,15 @@
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4  flex gap-4 flex-col md:flex-row'>
+      {/* left */}
+
+      <div className="w-full lg:w-2/3">l</div>
+      {/* right  */}
+      <div className="w-full lg:w-1/3">r</div>
+
+
+
+      </div>
   )
 }
 
