d00f85af15f470eadc59da807f6ebd5111a4b880
mdarf <mdarfinji45@gmail.com>
2024-11-04 01:42:15 +0800

    added search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
+import Image from "next/image"
+
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between'>
 
+{/* searchbar */}
 
-        
+<div className="hidden md:flex">
+    <Image src='/search.png' alt=""  width={14} height={14}/>
+    <input type="text" name="" id="" placeholder="search..." />
+</div>
+
+{/* icons and user */}
+
+<div className="">
+    <div className=""></div>
+</div>
+
     </div>
   )
 }
