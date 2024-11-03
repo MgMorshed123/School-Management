5e995bd31fd7f8e22762e4a951fc8aa4610c270d
mdarf <mdarfinji45@gmail.com>
2024-11-04 01:33:58 +0800

    responsive css addded to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -121,13 +121,13 @@ const Menu = () => {
     <div className=''>
       {
         menuItems.map((i)  => (
-          <div className="" key={i.title}>
-            <span>{i.title}</span>
+          <div className="mt-4 text-sm" key={i.title}>
+            <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
             {
               i.items.map((item) => (
-                <Link href={item.href} key={item.label}>
+                <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
                   <Image src={item.icon} alt="" width={20}height={20}></Image>
-                  <span>{item.label}</span>
+                  <span className="hidden lg:block" >{item.label}</span>
                 </Link>
               ))
             }
