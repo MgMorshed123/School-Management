275e5ef36bcd36528c4b6af74f1c52d8d9fd51aa
mdarf <mdarfinji45@gmail.com>
2024-11-04 02:13:54 +0800

    added notification number

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between'>
+    <div className='flex items-center justify-between mt-3'>
 
 {/* searchbar */}
 
@@ -12,18 +12,20 @@ const Navbar = () => {
 </div>
 
 {/* icons and user */}
-
 <div className="flex items-center gap-6">
     <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
         <Image  src='/message.png' alt="" width={20} height={20} />
     </div>
-    <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+    <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
         <Image  src='/announcement.png' alt="" width={20} height={20} />
+
+        <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-400 text-xs rounded-full">1</div>
+
     </div>
 
     <div className="flex flex-col">
         <span className="text-xs leading-3 font-medium">John Doe</span>
-        <span className="text-[10px] text-gray-500 text-right"></span>
+        <span className="text-[10px] text-gray-500 "></span>
     </div>
     <Image src='/avatar.png' alt="" width={36} height={36}  className="rounded-full"/>
 </div>
